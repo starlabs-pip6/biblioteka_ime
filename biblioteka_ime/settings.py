87ffb34f82f84c28eea225eee5ead5efe2408314
starlabs-pip6 <starlabs.pip6@gmail.com>
2021-06-22 11:36:56 +0200

    User Model
    User Model Created

--- biblioteka_ime/settings.py
+++ biblioteka_ime/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'libri_im',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'biblioteka_ime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'biblioteka_ime',
+        'USER': 'postgres',
+        'PASSWORD':'root',
+        'HOST': 'localhost',
+
     }
 }
 
--- libri_im/models.py
+++ libri_im/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class librat(models.Model):
+    id_libri=models.AutoField(primary_key=True)
+    isbn=models.IntegerField()
+    titulli=models.CharField(max_length=500)#char
+    autori=models.CharField(max_length=100) #char
+    kategoria=models.CharField(max_length=100)#char
+    pershkrimi=models.TextField()#char
+    mes_vleresimit=models.DecimalField(max_digits=5, decimal_places=2)#double
+    nr_vleresimit=models.IntegerField()#int
+    nr_faqeve=models.IntegerField()#int
+    viti_publikimit=models.IntegerField() #int
+
+    def __str__(self):
+        return self.titulli
