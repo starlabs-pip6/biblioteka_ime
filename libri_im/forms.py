2eeb628badb3047f64112afdab90c26c5c15121c
LirakR <lirakrushiti1@gmail.com>
2021-06-24 13:03:42 +0200

    new: Login and Logout functionality

--- libri_im/forms.py
+++ libri_im/forms.py
@@ -25,3 +25,26 @@ class RegistrationForm(UserCreationForm):
         except Exception:
            return username
         raise forms.ValidationError(f'Email {user.username} is already in use.')
+
+class UserAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = NewUser
+        fields = ('email', 'password')
+    
+    def clean(self):
+        if self.is_valid:
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email,password=password):
+                raise forms.ValidationError("Invalid login. Check email and password again.")
+
+    # def save(self):
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+    #     user = aithenticate(email=email, password=password)
+    #     if user:
+    #         login(request, user)
+
+    
--- libri_im/urls.py
+++ libri_im/urls.py
@@ -4,10 +4,17 @@ from . import views
 
 urlpatterns = [
     # Temporary endpoints to test serializers
-    path('backend/', views.backendOverView, name='home'),
+    path('backend/', views.backendOverView, name='backendView'),
     path('backend/books', views.booksList, name='booksList'),
     path('backend/books/<str:pk>/', views.specificBook, name='specificBook'),
-    path('register/', views.register_view, name='register')
+
+
+    path('', views.home_view, name="home"),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+
+
 ]
 
 
