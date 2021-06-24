5442d84b13ea26914d0e85b7692e80ed2817e6b0
3mper0r <83674499+3mper0r@users.noreply.github.com>
2021-06-24 10:04:29 +0200

    mesazhi

--- libri_im/urls.py
+++ libri_im/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     # Temporary endpoints to test serializers
-    path('backend/', views.backendOverView, name='home'),
-    path('backend/books', views.booksList, name='booksList'),
+    path('backend/', views.backendOverView, name=''),
+    path('backend/books', views.booksList, name='home'),
     path('backend/books/<str:pk>/', views.specificBook, name='specificBook'),
     path('register/', views.register_view, name='register')
 ]
