422995def8e983a1efded7a891c7c973545e60b1
Vlera Bllaca <vlerabll12@gmail.com>
2021-07-02 12:03:45 +0200

    Fix: updated directories

--- libri_im/urls.py
+++ libri_im/urls.py
@@ -17,19 +17,19 @@ urlpatterns = [
     
     #password reset
     path('reset_password/',
-     auth_views.PasswordResetView.as_view(template_name="libri_im/password_reset.html"),
+     auth_views.PasswordResetView.as_view(template_name="password_reset/password_reset.html"),
      name="reset_password"),
 
     path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name="libri_im/password_reset_sent.html"), 
+        auth_views.PasswordResetDoneView.as_view(template_name="password_reset/password_reset_sent.html"), 
         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(template_name="libri_im/password_reset_form.html"), 
+     auth_views.PasswordResetConfirmView.as_view(template_name="password_reset/password_reset_form.html"), 
      name="password_reset_confirm"),
 
     path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name="libri_im/password_reset_done.html"), 
+        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset/password_reset_done.html"), 
         name="password_reset_complete"),
 
     #Create
