d8ca62947556f27a5f9d8d5b65d7a85655b75526
LirakR <lirakrushiti1@gmail.com>
2021-07-06 13:47:29 +0200

    new: Profile pic for user accounts

--- biblioteka_ime/settings.py
+++ biblioteka_ime/settings.py
@@ -130,10 +130,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media')
+]
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
 
-#STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+BASE_DIR = "http://127.0.0.1:8000"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
