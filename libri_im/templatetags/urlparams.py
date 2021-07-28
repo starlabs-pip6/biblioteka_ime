8eafb1f3ba3187d39368aa3f8cdd2a1d39749f3c
VleraBllacaa <vlerabll12@gmail.com>
2021-07-28 12:32:57 +0200

    Fix: querystring parameter append

--- libri_im/templatetags/urlparams.py
+++ libri_im/templatetags/urlparams.py
@@ -1,10 +1,10 @@
 from django import template
-from urllib.parse import urlencode
+from django.utils.http import urlencode
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
-    query = context['request'].GET.copy()
+    query = context['request'].GET.dict()
     query.update(kwargs)
-    return query.urlencode()
+    return urlencode(query)
