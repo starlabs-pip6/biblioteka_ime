d122318a12298bfdad47b2430e487a7408ef0dd5
fatlum xhafa <fleksinho@hotmail.com>
2021-08-19 20:29:53 +0200

    added required urls

--- libri_im/urls.py
+++ libri_im/urls.py
@@ -7,122 +7,117 @@ from .views import CommentEditView,CommentDeleteView,BookCreateView, BookListVie
 
 urlpatterns = [
 
-    path('', views.home_view, name='home'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.RegistationView.as_view(), name='register'),
-    path('shfleto/', views.shfleto_view, name='shfleto'),
-    path('profile/view/', views.ProfilePageViewDetails, name="profile_page_view"),
-    path('libri/<str:isbn>', BookDV.as_view(), name='book-detail'),
-    path('user/survey', views.userSurvey, name='survey'),
-    path('events', views.eventsView, name='events'),
+    path('team6/', views.home_view, name='home'),
+    path('team6/login/', views.login_view, name='login'),
+    path('team6/logout/', views.logout_view, name='logout'),
+    path('team6/register/', views.RegistationView.as_view(), name='register'),
+    path('team6/shfleto/', views.shfleto_view, name='shfleto'),
+    path('team6/profile/view/', views.ProfilePageViewDetails, name="profile_page_view"),
+    path('team6/libri/<str:isbn>', BookDV.as_view(), name='book-detail'),
+    path('team6/user/survey', views.userSurvey, name='survey'),
+    path('team6/events', views.eventsView, name='events'),
 
 
 
-    path('home1/', views.home1,name="home1"),
-    path('home1/profile', views.ProfilePageViewDetails,name="profile1"),
-    path('login1/', views.login_view, name="login1"),
-   path('register1/', views.RegistationView.as_view(), name='register1'),
-    path('discover1/',views.discover1, name="discover1"),
-    path('backend1/home1/',
+    path('team6/home1/', views.home1,name="home1"),
+    path('team6/home1/profile', views.ProfilePageViewDetails,name="profile1"),
+    path('team6/login1/', views.login_view, name="login1"),
+   path('team6/register1/', views.RegistationView.as_view(), name='register1'),
+    path('team6/discover1/',views.discover1, name="discover1"),
+    path('team6/backend1/home1/',
          BookListView.as_view(template_name="backend1/admin_home1.html"), name="admin_home1"),
-    path('backend1/book1/<int:pk>/', BookDetailView.as_view(
+    path('team6/backend1/book1/<int:pk>/', BookDetailView.as_view(
         template_name="backend1/book_detail1.html"), name='book_detail1'),
-      path('backend1/book1/update1/<int:pk>/',
+      path('team6/backend1/book1/update1/<int:pk>/',
          EditBook.as_view(template_name="backend1/book_update1.html"), name='book_update1'),
-      path('backend1/book1/create1/',
+      path('team6/backend1/book1/create1/',
          BookCreateView.as_view(template_name="backend1/book_create1.html"), name='create1'),
-          path('backend1/book1/delete1/<int:pk>/',
+          path('team6/backend1/book1/delete1/<int:pk>/',
          BookDeleteView.as_view(template_name="backend1/delete1.html"), name='delete1'),
 
     #Comment likes and dislikes and replies
-    path('libri/<str:isbn>/comment/<int:pk>/reply', CommentReplyView.as_view(), name="comment-reply"), 
-    path('libri/<str:isbn>/comment/<int:pk>/like', AddCommentLike.as_view(), name="comment-like"), 
-    path('libri/<str:isbn>/comment/<int:pk>/dislike', AddCommentDislike.as_view(), name="comment-dislike"), 
-    path('libri/<str:isbn>/comment/<int:pk>/childlike', AddChildCommentLike.as_view(), name="child-comment-like"), 
-    path('libri/<str:isbn>/comment/<int:pk>/childdislike', AddChildCommentDislike.as_view(), name="child-comment-dislike"), 
-    path('libri/<str:isbn>/comment/commentdelete/<int:pk>/', CommentDeleteView.as_view(), name="comment-delete"),
-    path('libri/<str:isbn>/comment/commentedit/<int:pk>/', CommentEditView.as_view(), name="comment-edit"),
+    path('team6/libri/<str:isbn>/comment/<int:pk>/reply', CommentReplyView.as_view(), name="comment-reply"), 
+    path('team6/libri/<str:isbn>/comment/<int:pk>/like', AddCommentLike.as_view(), name="comment-like"), 
+    path('team6/libri/<str:isbn>/comment/<int:pk>/dislike', AddCommentDislike.as_view(), name="comment-dislike"), 
+    path('team6/libri/<str:isbn>/comment/<int:pk>/childlike', AddChildCommentLike.as_view(), name="child-comment-like"), 
+    path('team6/libri/<str:isbn>/comment/<int:pk>/childdislike', AddChildCommentDislike.as_view(), name="child-comment-dislike"), 
+    path('team6/libri/<str:isbn>/comment/commentdelete/<int:pk>/', CommentDeleteView.as_view(), name="comment-delete"),
+    path('team6/libri/<str:isbn>/comment/commentedit/<int:pk>/', CommentEditView.as_view(), name="comment-edit"),
 
      #friend system
-    path('find/friends/', views.findFriends, name="findFriends"),
-    path('profile/view/<int:pk>', ProfileDetailView.as_view(), name="profile-view"),
-    path('friend_request/',views.send_friend_request,name="friend-request"),
+    path('team6/find/friends/', views.findFriends, name="findFriends"),
+    path('team6/profile/view/<int:pk>', ProfileDetailView.as_view(), name="profile-view"),
+    path('team6/friend_request/',views.send_friend_request,name="friend-request"),
     # password reset - write your email to send you a link
-    path('reset_password/',
+    path('team6/reset_password/',
          auth_views.PasswordResetView.as_view(
              template_name="password_reset/password_reset.html"),
          name="reset_password"),
      #message that tells you that an email is sent to you
-    path('reset_password_sent/',
+    path('team6/reset_password_sent/',
          auth_views.PasswordResetDoneView.as_view(
              template_name="password_reset/password_reset_sent.html"),
          name="password_reset_done"),
      #write your new password and confirm it
-    path('reset/<uidb64>/<token>/',
+    path('team6/reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
              template_name="password_reset/password_reset_form.html"),
          name="password_reset_confirm"),
      #your password is change, now login with a new password
-    path('reset_password_complete/',
+    path('team6/reset_password_complete/',
          auth_views.PasswordResetCompleteView.as_view(
              template_name="password_reset/password_reset_done.html"),
          name="password_reset_complete"),
 
-    path('activate/<uidb64>/<token>',
+    path('team6/activate/<uidb64>/<token>',
          views.VerificationView.as_view(), name='activate'),
 
      #admin_home
-    path('backend/home/',
+    path('team6/backend/home/',
          BookListView.as_view(template_name="backend/admin_home.html"), name="admin_home"),
      #create a book - admin_home
-    path('backend/book/create/',
+    path('team6/backend/book/create/',
          BookCreateView.as_view(template_name="backend/book_create.html"), name='create'),
      #update a book - admin_home
-    path('backend/book/update/<int:pk>/',
+    path('team6/backend/book/update/<int:pk>/',
          EditBook.as_view(template_name="backend/book_update.html"), name='book_update'),
      #view details of a book - admin_home
-    path('backend/book/<int:pk>/', BookDetailView.as_view(
+    path('team6/backend/book/<int:pk>/', BookDetailView.as_view(
         template_name="backend/book_detail.html"), name='book_detail'),
      #delete a book - admin_home
-    path('backend/book/delete/<int:pk>/',
+    path('team6/backend/book/delete/<int:pk>/',
          BookDeleteView.as_view(template_name="backend/delete.html"), name='delete'),
      
-    path('profile/', ProfilePageView.as_view(template_name="libri_im/profile_page.html"),
+    path('team6/profile/', ProfilePageView.as_view(template_name="libri_im/profile_page.html"),
          name="profile_page"),
-    path('profile/update/', EditProfile.as_view(
+    path('team6/profile/update/', EditProfile.as_view(
         template_name="libri_im/profile_page_update.html"), name='profile_page_update'),
-    path('profile/password', views.MyPasswordChangeView.as_view(
+    path('team6/profile/password', views.MyPasswordChangeView.as_view(
         template_name="registration/change-password.html"), name="change-password"),
 
-    path('profile/view', views.ProfilePageViewDetails, name="profile_page_view"),
+    path('team6/profile/view', views.ProfilePageViewDetails, name="profile_page_view"),
 
 
     # POST DATA URLS
-    path('postdata/wtr', views.wantToReadPost, name='post_wtr'),
-    path('postdata/reading', views.ReadingPost, name='post_reading'),
-    path('postdata/read', views.ReadPost, name='post_read'),
-    path('postdata/progress', views.progressPost, name='post_progress'),
-    path('postdata/selectbook', views.selectBookPost, name='post_selectBook'),
-    path('postdata/friendrequest', views.friendRequestPost, name='post_friendrequest'),
-    path('postdata/category', views.categoryPost, name='post_category'),
-    path('postdata/events', views.post_addevent, name='post_addevent'),
+    path('team6/postdata/wtr', views.wantToReadPost, name='post_wtr'),
+    path('team6/postdata/reading', views.ReadingPost, name='post_reading'),
+    path('team6/postdata/read', views.ReadPost, name='post_read'),
+    path('team6/postdata/progress', views.progressPost, name='post_progress'),
+    path('team6/postdata/selectbook', views.selectBookPost, name='post_selectBook'),
+    path('team6/postdata/friendrequest', views.friendRequestPost, name='post_friendrequest'),
+    path('team6/postdata/category', views.categoryPost, name='post_category'),
+    path('team6/postdata/events', views.post_addevent, name='post_addevent'),
     
      
     # GET DATA URLS
-    path('getdata/wtr', views.getdataWtr, name="get_wtr"),
-    path('getdata/reading', views.getdataReading, name="get_reading"),
-    path('getdata/read', views.getdataRead, name="get_read"),
-    path('getdata/progress', views.getdataProgress, name="get_progress"),
-    path('getdata/selectbook', views.getdataSelectBook, name="get_selectBook"),
-    path('getdata/friendRequest', views.getdataFriendRequest, name="get_friendrequest"),
-    path('getdata/searched', views.getSearched, name="get_searched"),
-    path('getdata/category', views.getCategory, name="get_category"),
-    path('getdata/events', views.getEvents, name="get_events"),
-
-   
-
-
-
+    path('team6/getdata/wtr', views.getdataWtr, name="get_wtr"),
+    path('team6/getdata/reading', views.getdataReading, name="get_reading"),
+    path('team6/getdata/read', views.getdataRead, name="get_read"),
+    path('team6/getdata/progress', views.getdataProgress, name="get_progress"),
+    path('team6/getdata/selectbook', views.getdataSelectBook, name="get_selectBook"),
+    path('team6/getdata/friendRequest', views.getdataFriendRequest, name="get_friendrequest"),
+    path('team6/getdata/searched', views.getSearched, name="get_searched"),
+    path('team6/getdata/category', views.getCategory, name="get_category"),
+    path('team6/getdata/events', views.getEvents, name="get_events"),
 
 ]
